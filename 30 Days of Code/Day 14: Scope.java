7235bb55215e0abfeddf021844df95862f9360a5
Eshan Chaturvedi <33990363+EthanC-8@users.noreply.github.com>
2019-01-08 18:15:33 +0530

    Create Day 14: Scope.java

---
+++ 30 Days of Code/Day 14: Scope.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+
+	// Add your code here
+public Difference(int[] a) {
+  elements = a;
+}
+
+public void computeDifference() {
+  int max = 0;
+  for (int i = 0; i < elements.length-1; i++) {
+      for (int j = i+1; j < elements.length; j++) {
+          int diff = Math.abs(elements[i] - elements[j]);
+          if (diff > max) 
+              max = diff;
+      }
+  }
+  maximumDifference = max;
+}
+} // End of Difference class
